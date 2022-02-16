745394a9cfbb92fabe0f7c327c7c144c2283f877
3378950 <374008063@qq.com>
2022-02-16 12:58:58 +0800

    rebuild

--- src/main/java/com/zcs/space/controller/DefaultController.java
+++ src/main/java/com/zcs/space/controller/DefaultController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class DefailtController {
+public class DefaultController {
 
     @GetMapping
     public String sayHello() {
